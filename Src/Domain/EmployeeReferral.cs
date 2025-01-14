949050da05ef3c1d5057c813f5c43d395c983a87
Gustavo Borjas <gustavodanielborjas@outlook.com>
2025-01-14 16:47:16 -0300

    first commit

---
+++ Src/Domain/EmployeeReferral.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class EmployeeReferral
+    {
+        [Key]
+        public int Id { get; set; }
+        // Applicant information
+        public string ApplicantFirstName { get; set; }
+        public string ApplicantLastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // Referred position
+        public string ReferredPositionNameAndTitle { get; set; }
+
+        // Referred by
+        public string ReferrerFirstName { get; set; }
+        public string ReferrerLastName { get; set; }
+        public string ReferrerTitle { get; set; }
+        public string ReferrerDepartment { get; set; }
+        public string ReferrerEmail { get; set; }
+        public string ReferrerPhone { get; set; }
+
+        // Address
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+
+        // Signature URL
+        public string SignatureUrl { get; set; }
+    }
+}
